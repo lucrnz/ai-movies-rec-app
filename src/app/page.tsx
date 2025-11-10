fd237db661a802da5dde0ee91bb5ae271c4eb894
lucdev <me@lucdev.net>
2025-11-10 14:42:38 +0000

    feat: add footer & example landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { Footer, FOOTER_POSITIONS } from "@/features/footer/footer";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-96">
+        <h1 className="text-4xl font-display font-semibold">
+          Movie Recommendations App
+        </h1>
+        <p className="text-lg font-body">Know what movies to watch next</p>
+      </div>
+      <Footer position={FOOTER_POSITIONS.BOTTOM_FORCED} />
+    </>
+  );
 }
---
+++ src/features/site-consts/index.ts
@@ -0,0 +1,8 @@
+export const SITE_AUTHOR = "Luciano Hillcoat";
+export const SITE_AUTHOR_URL = "https://lucdev.net";
+export const SITE_NAME = "Movie Recommendations App";
+export const SITE_DESCRIPTION =
+  "Know what movies to watch next with our specialized AI";
+
+export const SITE_GITHUB_URL =
+  "https://github.com/lucrnz/movie-recommendations-app";
